eb280b446dca6aaae69594aba3c8c9907d69742e
schlenkibus <justus.scheil@parteimail.de>
2018-01-26 22:36:16 +0100

    added networking with udp

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <thread>
 #include "Application.h"
 #include "Tools/TimeUtils.h"
 #include "GameStates/MenuGameState.h"
+#include "SFML/Network.hpp"
 
 Application::Application() : m_window(sf::VideoMode(1366,768), "GGJ", sf::Style::Fullscreen),
                              currentState{}, m_resourceManager{} {
@@ -12,15 +14,41 @@ Application& Application::get() {
     return theApp;
 }
 
-
 int Application::run() {
     currentState.reset(std::make_unique<MenuGameState>().release());
+
+    std::thread network([](){
+        std::string ip("10.3.35.173");
+        sf::UdpSocket socketSend;
+        sf::UdpSocket socketRec;
+        socketRec.bind(22222);
+        socketRec.setBlocking(false);
+
+        socketSend.bind(55555);
+
+        char buffer[1024];
+        std::size_t received = 0;
+        auto test = "HELLO";
+        sf::IpAddress sender;
+        unsigned short port;
+
+        while(Application::get().getWindow().isOpen()) {
+            
+            socketSend.send(test, sizeof(test), ip, 55555);
+            if (socketRec.receive(buffer, sizeof(buffer), received, sender, port) == sf::Socket::Done)
+                std::cout << sender.toString() << " said: " << buffer << '\n';
+            else
+                std::cout << "nothing!\n";
+
+        }
+    });
 
     sf::Event event;
     while(m_window.isOpen()) {
         while(m_window.pollEvent(event)) {
             if(event.type == sf::Event::Closed) {
                 m_window.close();
+                network.join();
                 quit();
                 return 0;
             }
@@ -30,6 +58,7 @@ int Application::run() {
         draw();
         m_window.display();
     }
+    network.join();
 }
 
 void Application::update() {
@@ -47,8 +76,6 @@ void Application::draw() {
 }
 
 void Application::quit() {
-    //TODO install quitscreen
-    //currentState.reset(std::make_unique<QuitScreenState>().release());
     exit(0);
 }
 
